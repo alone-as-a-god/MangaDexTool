7013bb5db4a2b74d8df719f1bc3c1a5615705340
alone as a god <senoriship@gmail.com>
2023-07-09 21:59:05 +0200

    Messy code but loading all chapters works

--- MangaRequests.py
+++ MangaRequests.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import requests
 import pandas
 
@@ -12,14 +13,71 @@ def get_manga(title):
     return response.json()
 
 
-def get_chapters(manga_id):
+def get_full_feed(manga_id):
+    limit = 100
+    offset = 0
+
     response = requests.get(
         BASE_URL + f"/manga/{manga_id}/feed",
         params={"translatedLanguage[]": "en",
-                "order[chapter]": "asc"
+                "order[chapter]": "asc",
+                "offset": offset,
                 }
     )
-    data = pandas.DataFrame(response.json()["data"])
-    grouped_data = data.groupby(data["relationships"].apply(lambda x: x[0]["id"]))
+    data = response.json()
+    total = data["total"]
+    df = pandas.json_normalize(data["data"])
+    offset += limit
+    print(total)
 
-    return response.json()
+    while total > offset:
+        response = requests.get(
+            BASE_URL + f"/manga/{manga_id}/feed",
+            params={"translatedLanguage[]": "en",
+                    "order[chapter]": "asc",
+                    "offset": offset,
+                    }
+        )
+        data = response.json()
+        temp = pandas.json_normalize(data["data"])
+        df = pandas.concat([df, temp])
+        offset += limit
+
+    return df
+
+
+def get_chapters(manga_id):
+    df = get_full_feed(manga_id)
+    formatted_df = df.copy()
+
+    formatted_df = formatted_df.rename(columns={"attributes.volume": "volume", "attributes.chapter": "chapter",
+                                                "attributes.translatedLanguage": "language",
+                                                "attributes.pages": "pages",
+                                                "attributes.title": "title"
+                                                })
+
+    new_df = pandas.DataFrame()
+    for index, row in formatted_df.iterrows():
+        row["group"] = row["relationships"][0]["id"]
+        row = row.to_frame()
+        row = row.transpose()
+        new_df = pandas.concat([new_df, pandas.DataFrame(row)])
+    new_df = new_df.drop(
+        columns=["attributes.readableAt", "attributes.publishAt", "attributes.createdAt", "attributes.updatedAt",
+                 "attributes.externalUrl", "attributes.version", "relationships"])
+    new_df["chapter"] = new_df["chapter"].astype(float)
+    group_counts = new_df["group"].value_counts()
+
+    new_df["group"] = pandas.Categorical(new_df["group"], categories=group_counts.index, ordered=True)
+    new_df = new_df.sort_values(by=["chapter", "group"], ascending=[True, True])
+
+    chapter_list = []
+    chapters_df = pandas.DataFrame()
+
+    for index, row in new_df.iterrows():
+        if row["chapter"] not in chapter_list:
+            chapter_list.append(row["chapter"])
+            chapters_df = pandas.concat([chapters_df, pandas.DataFrame(row).transpose()])
+
+    print(chapters_df)
+    return chapters_df
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import MangaRequests
 
 if __name__ == "__main__":
     # title_input = input("Please enter a title: ")
-    title_input = "Solo Leveling"
-    mangas = MangaRequests.get_manga(title_input)
-    chapters = MangaRequests.get_chapters(mangas["data"][0]["id"])
-    for chapter in chapters["data"]:
-        print(chapter["attributes"]["chapter"])
+    # title_input = "Solo Leveling"
+    # mangas = MangaRequests.get_manga(title_input)
+    chapter = MangaRequests.get_chapters("32d76d19-8a05-4db0-9fc2-e0b0648fe9d0")
+    # chapters = MangaRequests.get_chapters(mangas["data"][0]["id"])
+    # for chapter in chapters["data"]:
+    #   print(chapter["attributes"]["chapter"])
